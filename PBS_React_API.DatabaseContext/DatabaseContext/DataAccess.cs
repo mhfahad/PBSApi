525092986c0e91c37c6952a7631e5b7b271a2b3d
Mahmud Hasan <m.hasan@panjeree.local>
2024-03-07 14:41:42 +0600

    20240307

---
+++ PBS_React_API.DatabaseContext/DatabaseContext/DataAccess.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.Graph;
+using PBS_React_API.DatabaseContext.Interface;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBS_React_API.DatabaseContext.DatabaseContext
+{
+    public class DataAccess : iDataAccess
+    {
+        public SqlConnection m_Conn;
+        private Hashtable m_Erroobj;
+        //public string DBConnstr = "Data Source=AST05\\MSSQLSVR2K5;initial Catalog=ASTSOFTDB;Integrated Security=SSPI";
+        public string DBConnstr = "Data Source=172.16.1.152;Initial Catalog=ECO;Persist Security Info=True;User ID=sa;Password=sa*1209";
+        public DataAccess()
+        {
+            m_Conn = new SqlConnection(this.DBConnstr);
+            m_Erroobj = new Hashtable();
+        }
+
+        public DataAccess(string mDBName)
+        {
+            m_Conn = new SqlConnection(this.DBConnstr);
+            m_Conn.ConnectionString = m_Conn.ConnectionString.Replace("ECO", mDBName );
+
+        }
+
+        
+        public Hashtable ErrorObject
+        {
+            get
+            {
+                return this.m_Erroobj;
+            }
+        }
+        public DataTable GetTable(string SQl)
+        {
+            //for string : have to edit with proper code
+            try
+            {
+                SqlDataAdapter adp = new SqlDataAdapter();
+                DataSet ds = new DataSet();
+                adp.Fill(ds);
+                return ds.Tables[0];
+            }
+
+            catch (Exception ex)
+            {
+                this.SetError(ex);
+                return null;
+            }
+        }
+        public DataSet GetDataSet(String SQL)
+        {
+            //for string : have to edit with proper code
+            try
+            {
+                SqlDataAdapter adp = new SqlDataAdapter(SQL, this.m_Conn);
+                DataSet ds = new DataSet();
+                adp.Fill(ds);
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                this.SetError(ex);
+                return null;
+            }
+        }
+        public DataTable GetTable(SqlCommand Cmd)
+        {
+            try
+            {
+                SqlDataAdapter adp = new SqlDataAdapter();
+                adp.SelectCommand = Cmd;
+                Cmd.Connection = this.m_Conn;
+                DataTable dt = new DataTable();
+                adp.Fill(dt);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                this.SetError(ex);
+                return null;
+            }
+        }
+        public DataSet GetDataSet(SqlCommand Cmd)
+        {
+            try
+            {
+                SqlDataAdapter adp = new SqlDataAdapter();
+                adp.SelectCommand = Cmd;
+
+                Cmd.CommandTimeout = 120;
+                Cmd.Connection = this.m_Conn;
+                DataSet ds = new DataSet();
+                adp.Fill(ds);
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                this.SetError(ex);
+                return null;
+            }
+        }
+        
+        public Boolean ExecuteCommand(string SQL)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = SQL;
+            cmd.Connection = this.m_Conn;
+
+            try
+            {
+                this.m_Conn.Open();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                this.SetError(ex);
+                return false;
+            }
+            finally
+            {
+                this.m_Conn.Close();
+            }
+        }
+
+
+        public Boolean ExecuteCommand(SqlCommand cmd)
+        {
+            cmd.Connection = this.m_Conn;
+            try
+            {
+                this.m_Conn.Open();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                this.SetError(ex);
+                return false;
+            }
+            finally
+            {
+                this.m_Conn.Close();
+            }
+        }
+        public SqlDataReader ExecuteReader(SqlCommand cmd)
+        {
+            cmd.Connection = this.m_Conn;
+            cmd.CommandTimeout = 120;
+            try
+            {
+                this.m_Conn.Close();
+                this.m_Conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                return reader;
+            }
+            catch (Exception ex)
+            {
+                this.SetError(ex);
+                return null;
+            }
+
+        }
+        public void SetError(Exception ex)
+        {
+            this.m_Erroobj["Src"] = ex.Source;
+            this.m_Erroobj["Msg"] = ex.Message;
+            this.m_Erroobj["Location"] = ex.StackTrace;
+        }
+    }
+}
---
+++ PBS_React_API/Controllers/DataSyncController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PBS_React_API.DatabaseContext.Interface;
+using PBS_React_API.Model;
+using System.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PBS_React_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataSyncController : ControllerBase
+    {
+
+        private readonly iProjectDataContext _dataContext;
+
+        public DataSyncController( iProjectDataContext dataContext)
+        {
+           
+            _dataContext = dataContext;
+        }
+
+
+        [HttpGet("GET_AUTOCOMPLETE_DATA_QUICK_SYNC")]
+        public async Task<APIServiceResponse> GET_AUTOCOMPLETE_DATA_QUICK([FromHeader(Name = "pass")] string pass)
+        {
+            APIServiceResponse objResponse = new APIServiceResponse();
+            try
+            {
+                DataSet ds = _dataContext.GetTransInfo("TestFahad", "GET_AUTOCOMPLETE_DATA_QUICK_SYCN");
+                ds.WriteXml(@"QuickSearchData.xml");
+
+                objResponse.ResponseBusinessData = JsonConvert.SerializeObject("Ok").ToString();
+                objResponse.ResponseCode = 200;
+                objResponse.SuccessMsg = "DATA SYNC SUCCESSFULLY";
+
+                return objResponse;
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet("GET_AUTOCOMPLETE_DATA_QUICK")]
+        public async Task<APIServiceResponse> GET_AUTOCOMPLETE_DATA([FromHeader(Name = "pass")] string pass,string SerText)
+        {
+            APIServiceResponse objResponse = new APIServiceResponse();
+            try
+            {
+
+                DataSet ds1 = new DataSet();
+                ds1.ReadXml(@"QuickSearchData.xml");
+
+                DataView dv = new DataView();
+
+                dv.Table = ds1.Tables[0];
+                dv.RowFilter = "name like '%" + SerText + "%' or searchingkeyword like '%" + SerText + "%' or NameBN like '%" + SerText + "%' or AuthorNameBn like '%" + SerText + "%' or PublisherNameBN like '%" + SerText + "%'";
+
+                DataTable dt = (dv.ToTable()).Rows.Cast<System.Data.DataRow>().Take(20).CopyToDataTable();
+
+                objResponse.ResponseBusinessData = JsonConvert.SerializeObject(dt);
+                objResponse.ResponseCode = 200;
+                return objResponse;
+
+
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
